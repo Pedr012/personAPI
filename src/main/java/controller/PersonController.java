1aa4649bce34a2c2d22e07c32eb129280f20545c
Pedro Rocha <pedrohrrf17@gmail.com>
2021-08-10 14:07:48 -0300

    finished project

---
+++ src/main/java/controller/PersonController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import dto.MessageResponseDTO;
+import entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import repository.PersonRepository;
+
+@RestController
+@RequestMapping("ap1/v1/pepople")
+public class PersonController {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        Person savedPerson = personRepository.save(person);
+
+        return MessageResponseDTO.builder().message("Created person with ID" ).build();
+    }
+}
--- src/main/java/one/digitalinnovation/personapi/entity/Phone.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -1,12 +1,19 @@
-package entity;
+package one.digitalinnovation.personapi.entity;
 
-import enums.PhoneType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import one.digitalinnovation.personapi.enums.PhoneType;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Entity
 @Data
 @Builder
@@ -16,7 +23,7 @@ public class Phone {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
@@ -24,5 +31,6 @@ public class Phone {
 
     @Column(nullable = false)
     private String number;
+
 
 }
